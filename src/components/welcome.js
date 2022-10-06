8e689534fef13c0de3d54d6628828816a87fe1c6
travis-mcginley <travis.o.mcginley@gmail.com>
2022-10-05 21:02:02 -0400

    Add initial animations to welcome page

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,8 +1,59 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../styles/components/welcome.css';
 
+// Fade words in
+// Then show scroll botton/ bar
+
 const Welcome = () => {
-  return <div className="container">Welcome</div>;
+  // const [message, setMessage] = useState('');
+
+  // const phrases = [
+  //   'Welcome',
+  //   'It is time...',
+  //   'To Rock out \\m/',
+  // ];
+
+  // let i = 0;
+  // const print = () => {
+  //   setTimeout(() => {
+  //     console.log('hello');
+  //     i += 1;
+
+  //     if (i < phrases.length) {
+  //       print();
+  //     }
+  //   }, 3000);
+  // };
+
+  const printMessage = () => {
+    // Add class to message to fade in
+    // Check transition complete
+    // Add class to message to fade out
+    // Check transition complete
+    // Go to next phrase
+
+    const messageNode = document.getElementById('fade');
+    messageNode.className = 'in';
+
+    messageNode.innerText = 'Hello World';
+    messageNode.addEventListener('animationend', () => {
+      messageNode.className = 'out';
+    });
+  };
+
+  useEffect(() => {
+    document.addEventListener('load', printMessage());
+
+    return () => {
+      document.removeEventListener('load', printMessage());
+    };
+  }, []);
+
+  return (
+    <div className="container">
+      <span id="fade" />
+    </div>
+  );
 };
 
 export default Welcome;
